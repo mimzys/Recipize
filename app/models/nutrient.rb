fd38482430ddf3f8170b48af9866b4740168f01c
Sasha Grinev <sashagrinev@Elenas-MacBook-Air.local>
2019-02-18 17:14:57 -0500

    add nutrient model test

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -3,6 +3,8 @@ class Nutrient < ApplicationRecord
   belongs_to :macro, class_name: "Nutrient", optional: true
   validates_presence_of :daily, :label, :tag, :total,
     :unit, :recipe_id
-  validates_numericality_of :daily, :total
+  validates_numericality_of :daily, greater_than_or_equal_to: 0
+  validates_numericality_of :total, greater_than_or_equal_to: 0
+  validates_inclusion_of :hasRDI, :in => [true, false]
   belongs_to :recipe
 end
---
+++ spec/models/nutrient_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Nutrient do
+
+  it { should have_valid(:daily).when(1, 3.4, "1.2", "10", 0) }
+  it { should_not have_valid(:daily).when(nil, "", -1) }
+
+  it { should have_valid(:hasRDI).when(false, true) }
+  it { should_not have_valid(:hasRDI).when(nil, "") }
+
+  it { should have_valid(:label).when("anything") }
+  it { should_not have_valid(:label).when(nil, "") }
+
+  it { should have_valid(:schemaOrgTag).when("anything", nil, "") }
+
+  it { should have_valid(:tag).when("anything") }
+  it { should_not have_valid(:tag).when(nil, "") }
+
+  it { should have_valid(:total).when(1, 3.4, "1.2", "10", 0) }
+  it { should_not have_valid(:total).when(nil, "", -1) }
+
+  it { should have_valid(:unit).when("anything") }
+  it { should_not have_valid(:unit).when(nil, "") }
+
+  Recipe.new(label: "test", image: "animage.jpg", totalTime: 10, calories: 300)
+  it { should have_valid(:recipe_id).when(1) }
+  it { should_not have_valid(:recipe_id).when(nil, "") }
+
+  it { should belong_to(:recipe) }
+
+  Nutrient.new(daily: 34, hasRDI: true, label: "macro", tag: "banana", total: 542, unit: "mg", recipe_id: 1)
+  it { should have_valid(:macro_id).when(1, nil) }
+  it { should_not have_valid(:recipe_id).when("") }
+
+  it { should belong_to(:macro) }
+end
